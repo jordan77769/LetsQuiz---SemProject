c6178aa5e9e1d8901abe005f785852175f03020f
jordan77769 <jtorske.98@gmail.com>
2018-02-27 14:56:42 -0600

    Initial commit

---
+++ app/src/main/java/com/example/jtors/letsquiz/UserCreatedTopics.java
@@ -0,0 +1,13 @@
+package com.example.jtors.letsquiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UserCreatedTopics extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_created_topics);
+    }
+}
