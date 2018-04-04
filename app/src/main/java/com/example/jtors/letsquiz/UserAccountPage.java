f6175456f4c1e8cb699d6cbffe36b1f71b82dc9d
jordan77769 <jtorske.98@gmail.com>
2018-04-03 22:59:41 -0500

    Initial commit

--- app/src/main/java/com/example/jtors/letsquiz/UserAccountPage.java
+++ app/src/main/java/com/example/jtors/letsquiz/UserAccountPage.java
@@ -1,10 +1,17 @@
 package com.example.jtors.letsquiz;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 
 public class UserAccountPage extends AppCompatActivity {
 
@@ -20,5 +27,45 @@ public class UserAccountPage extends AppCompatActivity {
                 startActivity(new Intent(UserAccountPage.this, MainPage.class));
             }
         });
+
+        String filename = "myfile";
+        String fileContents = "Hello user!";
+        FileOutputStream outputStream;
+
+        try {
+           outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
+            outputStream.write(fileContents.getBytes());
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        FileInputStream inputStream;
+        String result = "";
+        try {
+            inputStream = openFileInput(filename);
+            result = "";
+            int element = 0;
+            while((element = inputStream.read()) != -1 )
+            {
+                result += (char)element;
+            }
+            inputStream.close();
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        Toast.makeText(this, result,Toast.LENGTH_SHORT).show();
+
+        //Get Directory
+        File directory = this.getFilesDir();
+        File[] list = directory.listFiles();
+        for(int i=0; i<list.length;i++)
+        {
+            File getFile = list[i];
+            Log.i("INFO", getFile.getAbsolutePath());
+        }
     }
 }
